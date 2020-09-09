d1a9b3b51edb9bf75f7a5601ab8c30c3a64b347a
parkyw1206 <36143152+parkyw1206@users.noreply.github.com>
2020-09-09 02:44:50 -0700

    Add files via upload

---
+++ sw_gravity.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N = 9, M = 8;
+    int first = 0 , countOne = 0, max = 0;
+    vector<int> v= {7,4,2,0,0,6,0,7,0};
+    int box[N][M];
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < v[i] ; j++){
+            box[i][j] = 1;
+        }
+        for(int j = v[i]; j < M; j++){
+            box[i][j] = 0;   
+        }
+    }
+    for(int i = 0; i < M ; i++){
+        first = 0;
+        countOne = N;
+        for(int j = 0 ; j < N ; j++){
+            if(box[j][i] == 1 && first != 0){
+                countOne--;
+            }
+            if(box[j][i] == 1){
+                if(first == 0)
+                    first = j;
+                countOne--;
+            }
+        }
+        if(first != 0 && max < countOne){
+            max = countOne;
+        }
+    }
+    
+    return max;
+}
